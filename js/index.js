9d9078f1e530f834f53779afd7fcc15e3ca9e535
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-01 11:57:39 +1100

    planning how to start building the game

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
 console.log("%c Project 0: TIC-TAC-TOE", "color: blue; background-color: yellow");
+
+/*
+RULES FOR TIC-TAC-TOE
+
+1. CSS FILE: The game is played on a grid that's 3 squares by 3 squares.
+
+2. JS LOGIC: You are X, your friend (or the computer in this case) is O. Players take turns putting their marks in empty squares.
+
+3. JS LOGIC: The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.
+
+4. JS LOGIC: When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
+
+---------------
+
+1. Separate HTML, CSS, and JavaScript files
+2. Create the HTML/CSS board/grid of game to play
+3. Create the js logic to play the tic tac toe
+4. Add a readme.md file with explanations of the technologies used, the approach taken, installation instructions, unsolved problems, etc.
+5. Improve the style of CSS sheet
+*/
+
