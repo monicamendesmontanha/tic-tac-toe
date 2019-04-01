33cfa5d9df7851ed3f0aa7b66bcac253e6732430
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-02 10:34:03 +1100

    clean-up js file

--- js/index.js
+++ js/index.js
@@ -1,21 +1,12 @@
-console.log(
-  "%c Project 0: TIC-TAC-TOE",
-  "color: blue; background-color: yellow"
-);
+console.log("%c Project 0: TIC-TAC-TOE", "color: blue; background-color: yellow");
 
-/*
-RULES FOR TIC-TAC-TOE
+/*RULES FOR TIC-TAC-TOE
 
 1. CSS FILE: The game is played on a grid that's 3 squares by 3 squares.
-
 2. JS LOGIC: You are X, your friend (or the computer in this case) is O. Players take turns putting their marks in empty squares.
-
 3. JS LOGIC: The first player to get 3 of her marks in a row (up, down, across, or diagonally) is the winner.
-
 4. JS LOGIC: When all 9 squares are full, the game is over. If no player has 3 marks in a row, the game ends in a tie.
-
 ---------------
-
 1. Separate HTML, CSS, and JavaScript files
 2. Create the HTML/CSS board/grid of game to play
 3. Create the js logic to play the tic tac toe
@@ -45,23 +36,21 @@ $(document).ready(() => {
 
       $(`#column-${i}-${j}`).click(function() {
 
-
         if (rounds % 2 !== 0) {
           matrix[i][j] = "X";
-          // $(`#column-${i}-${j} .square span`).addClass("nes-icon is-large heart");
           insideBalloon = "Your turn!"
-
+          // $(`#column-${i}-${j} .square span`).addClass("nes-icon is-large heart");
         } else {
           matrix[i][j] = "O";
-          // $(`#column-${i}-${j} .square span`).addClass("nes-icon is-large star");
           insideBalloon = "Wait!"
+          // $(`#column-${i}-${j} .square span`).addClass("nes-icon is-large star");
         }
 
         $(`#column-${i}-${j} .square`).text(matrix[i][j]);
         $( '#inside-balloon'  ).text(insideBalloon);
 
-
         rounds = rounds - 1;
+
       });
     }
   }
