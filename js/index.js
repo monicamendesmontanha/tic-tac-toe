f6490eb76d92459d062b34dc9e612561a35a6931
Monica Mendes <monicamendesmontanha@gmail.com>
2019-04-04 10:01:13 +1100

    clean-up html and css file

--- js/index.js
+++ js/index.js
@@ -10,29 +10,30 @@ const PLAYER_2 = "O";
 const EMPTY_SLOT = "-";
 
 const checkWinner = function(player) {
-
-  const showWin = function(selector){
-    $(selector).css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0},2000);
-  }
+  const showWin = function(selector) {
+    $(selector)
+      .css({ opacity: 0.0, visibility: "visible" })
+      .animate({ opacity: 1.0 }, 2000);
+  };
 
   const horizontal0 = [matrix[0][0], matrix[0][1], matrix[0][2]].every(
     item => item === player
   );
   if (horizontal0) {
-    showWin('.horizontal0');
+    showWin(".horizontal0");
   }
 
   const horizontal1 = [matrix[1][0], matrix[1][1], matrix[1][2]].every(
     item => item === player
   );
   if (horizontal1) {
-    showWin('.horizontal1');
+    showWin(".horizontal1");
   }
   const horizontal2 = [matrix[2][0], matrix[2][1], matrix[2][2]].every(
     item => item === player
   );
   if (horizontal2) {
-    showWin('.horizontal2');
+    showWin(".horizontal2");
   }
 
   const horizontal = [horizontal0, horizontal1, horizontal2].some(
@@ -43,19 +44,19 @@ const checkWinner = function(player) {
     item => item === player
   );
   if (vertical0) {
-    showWin('.vertical0');
+    showWin(".vertical0");
   }
   const vertical1 = [matrix[0][1], matrix[1][1], matrix[2][1]].every(
     item => item === player
   );
   if (vertical1) {
-    showWin('.vertical1');
+    showWin(".vertical1");
   }
   const vertical2 = [matrix[0][2], matrix[1][2], matrix[2][2]].every(
     item => item === player
   );
   if (vertical2) {
-    showWin('.vertical2');
+    showWin(".vertical2");
   }
 
   const vertical = [vertical0, vertical1, vertical2].some(
@@ -66,13 +67,13 @@ const checkWinner = function(player) {
     item => item === player
   );
   if (diagonalRight) {
-    showWin('.diagonalRight');
+    showWin(".diagonalRight");
   }
   const diagonalLeft = [matrix[0][0], matrix[1][1], matrix[2][2]].every(
     item => item === player
   );
   if (diagonalLeft) {
-    showWin('.diagonalLeft');
+    showWin(".diagonalLeft");
   }
 
   const diagonal = [diagonalRight, diagonalLeft].some(
@@ -126,13 +127,21 @@ const createGameplay = function() {
         if (rounds % 2 !== 0) {
           matrix[i][j] = PLAYER_1;
           insideBalloon = `${PLAYER_2}'s turn`;
-          $('input:radio[name="answer-dark"]').filter('[value="X"]').removeAttr('checked')
-          $('input:radio[name="answer-dark"]').filter('[value="O"]').attr('checked', true)
+          $('input:radio[name="answer-dark"]')
+            .filter('[value="X"]')
+            .removeAttr("checked");
+          $('input:radio[name="answer-dark"]')
+            .filter('[value="O"]')
+            .attr("checked", true);
         } else {
           matrix[i][j] = PLAYER_2;
           insideBalloon = `${PLAYER_1}'s turn`;
-          $('input:radio[name="answer-dark"]').filter('[value="O"]').removeAttr('checked')
-          $('input:radio[name="answer-dark"]').filter('[value="X"]').attr('checked', true)
+          $('input:radio[name="answer-dark"]')
+            .filter('[value="O"]')
+            .removeAttr("checked");
+          $('input:radio[name="answer-dark"]')
+            .filter('[value="X"]')
+            .attr("checked", true);
         }
 
         $(`#column-${i}-${j} .square`).text(matrix[i][j]);
@@ -140,38 +149,46 @@ const createGameplay = function() {
 
         if (checkWinner(PLAYER_1)) {
           $("#inside-balloon").text(`${PLAYER_1} won!`);
-          $( "i#characters" ).removeClass( "nes-squirtle" ).addClass( "nes-icon trophy is-large" );
+          $("i#characters")
+            .removeClass("nes-squirtle")
+            .addClass("nes-icon trophy is-large");
           gameEnded = true;
         }
 
         if (checkWinner(PLAYER_2)) {
           $("#inside-balloon").text(`${PLAYER_2} won!`);
-          $( "i#characters" ).removeClass( "nes-squirtle" ).addClass( "nes-icon trophy is-large" );
+          $("i#characters")
+            .removeClass("nes-squirtle")
+            .addClass("nes-icon trophy is-large");
           gameEnded = true;
         }
-        if (!checkWinner(PLAYER_1) && !checkWinner(PLAYER_2) && rounds >= 8){
+        if (!checkWinner(PLAYER_1) && !checkWinner(PLAYER_2) && rounds >= 8) {
           $("#inside-balloon").text(`It's a draw!`);
-          $( "i#characters" ).removeClass( "nes-squirtle" ).addClass( "nes-icon is-large star is-half" );
+          $("i#characters")
+            .removeClass("nes-squirtle")
+            .addClass("nes-icon is-large star is-half");
           gameEnded = true;
         }
 
         rounds = rounds + 1;
-
       });
     }
   }
 };
 
-const restartGame = function(){
+const restartGame = function() {
   $("#inside-balloon").text("Let's Start!");
-  $( "i#characters" ).removeClass().addClass( "nes-squirtle" );
+  $("i#characters")
+    .removeClass()
+    .addClass("nes-squirtle");
   $(".board").empty();
-  $(".result").css({opacity: 1.0, visibility: "hidden"}).animate({opacity: 0.0});
+  $(".result")
+    .css({ opacity: 1.0, visibility: "hidden" })
+    .animate({ opacity: 0.0 });
   rounds = 0;
   createBoard();
   createGameplay();
-
-}
+};
 
 $(document).ready(() => {
   restartGame();
@@ -180,7 +197,3 @@ $(document).ready(() => {
     restartGame();
   });
 });
-
-$('.horizontal0').css({'visibility': 'show'})
-$('.horizontal1').css({'visibility': 'show'})
-$('.horizontal2').css({'visibility': 'show'})
